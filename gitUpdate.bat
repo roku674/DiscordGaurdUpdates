319769f9682730ebf39a608fd86ddc3e788a2d0f
roku674 <af03848@georgiasouthern.edu>
2022-09-19 00:32:56 -0400

    rm files form .gitignore

--- gitUpdate.bat
+++
@@ -1,9 +0,0 @@
-git.exe fetch
-git.exe pull
-cd H:\My Drive\Shared\DiscordGaurdUpdates\\DiscordGaurdUpdates\Objects\StarportObjects
-git.exe fetch
-git.exe pull
-cd H:\My Drive\Shared\DiscordGaurdUpdates\\DiscordGaurdUpdates\\Algorithms-for-C-Sharp
-git.exe fetch
-git.exe pull
-@pause
